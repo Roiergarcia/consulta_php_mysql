8c70e64298b8a184f4a922f83c948b6735f2af4b
Roiergarcia <isc.rgm@gmail.com>
2014-02-23 00:28:39 -0600

    Create conector.php

---
+++ conector.php
@@ -0,0 +1,13 @@
+<?php
+/******** Autor: Rogelio Garcia ********************* */
+/******** Web: desarrollo-sistemas.com.mx *********** */
+/******** CONECTAR CON BASE DE DATOS **************** */
+/******** Recuerda cambiar por tus datos (host, usuario, password) ***********/ 
+   $con = mysql_connect('localhost','root','');
+   if (!$con){die('ERROR DE CONEXION CON MYSQL: '. mysql_error());}
+/* ****************************************************** */
+/* ******** CONECTA CON LA BASE DE DATOS  *************** */
+   $database = mysql_select_db('bd_personal',$con);
+   if (!$database){die('ERROR CONEXION CON BD: '.mysql_error());}
+/* ********************************************** */
+?>
